8f10d8e33ad92e3c5d506ba55b8617dd96b23ca5
KISHAN KUMAR <81502923+kishan779@users.noreply.github.com>
2023-07-05 00:22:37 +0530

    Time: 26 ms (43.16%), Space: 26.5 MB (30.53%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,36 +11,30 @@
  */
 class Solution {
 public:
-    TreeNode* buildBT(vector<int>& inorder, vector<int>& postorder, int inStart, int inEnd, int postStart, int postEnd) {
-        if(inStart > inEnd)
-            return NULL;
 
-        TreeNode* root = new TreeNode(postorder[postEnd]);
-        int root_candidate = root->val;
-        int i = inStart;
-      
-        //YOu can optimize this by storing index of inorder values in a map for direct access
-        for(; i<=inEnd; i++) {
-            if(inorder[i] == root_candidate) {
-                break;
-            }
-        }
-        int leftSize  = i-inStart;
-        int rightSize = inEnd-i;
-      
-        root->left = buildBT(inorder, postorder, inStart, i-1, postStart, postStart+leftSize-1);
-        root->right  = buildBT(inorder, postorder, i+1, inEnd, postEnd-rightSize, postEnd-1);
-      
-        return root;
-    }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        TreeNode* root = NULL;
-        int n          = postorder.size();
-        int inStart    = 0;
-        int inEnd      = n-1;
-        int postStart  = 0;
-        int postEnd    = n-1;
-        root           = buildBT(inorder, postorder, inStart, inEnd, postStart, postEnd);
+        if(inorder.size()!=postorder.size()){
+            return NULL;
+        }
+        map<int,int> inMap;
+        for(int i=0;i<inorder.size();i++){
+            inMap[inorder[i]]=i;
+        }
+       
+        return helper(inorder,0,inorder.size()-1,postorder,0,postorder.size()-1,inMap);
+    }
+
+        TreeNode* helper(vector<int>&inorder,int is,int ie,vector<int>&postorder,int ps,int pe,map<int,int> &hm){
+        if(is>ie || ps>pe){
+            return NULL;
+        }
+        TreeNode* root=new TreeNode(postorder[pe]);
+        int inRoot=hm[postorder[pe]];
+        int numsLeft=inRoot-is;
+
+        root->left=helper(inorder,is,inRoot-1,postorder,ps,ps+numsLeft-1,hm);
+        root->right=helper(inorder,inRoot+1,ie,postorder,ps+numsLeft,pe-1,hm);
         return root;
     }
+
 };
