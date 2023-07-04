57e011f2ba58e477f5b8fe4a1dd20cce7acb8c15
KISHAN KUMAR <81502923+kishan779@users.noreply.github.com>
2023-07-05 00:45:36 +0530

    Time: 19 ms (69.29%), Space: 25.9 MB (82.34%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -10,31 +10,21 @@
  * };
  */
 class Solution {
-public:
-
-    unordered_map<int, int>u;
-
-    TreeNode* build(vector<int>post, int &postidx, int si, int ei){
-      if(postidx < 0 || si > ei)
-      return NULL;
-
-      TreeNode* root =new TreeNode(post[postidx]);
-      postidx--;
-
-      int rootidx = u[root->val];
-      root ->right = build(post, postidx, rootidx+1,ei);
-      root ->left = build(post, postidx, si, rootidx-1);
-
-      return root;
+    TreeNode* buildTree_rec(vector<int>& inorder, int i1, int i2, vector<int>& postorder, int p1, int p2) {
+        if(i1 >= i2 || p1 >= p2) return nullptr;
+        TreeNode *root = new TreeNode(postorder[p2-1]);
+        auto it = find(inorder.begin() + i1, inorder.begin() + i2, postorder[p2-1]);
+        int diff = it - inorder.begin() - i1;
+        root->left = buildTree_rec(inorder, i1, i1 + diff, postorder, p1, p1 + diff);
+        root->right = buildTree_rec(inorder, i1 + diff + 1, i2, postorder, p1+diff, p2-1);
+        
+        return root;
     }
-
+public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-       for(int i =0; i< inorder.size(); i++){
-         u[inorder[i]] =i;
-       }
-
-       int postidx = postorder.size()-1;
-       return build(postorder, postidx,0,postorder.size()-1);
-       
+        int n = inorder.size();
+        if(n == 0) return nullptr;
+        
+        return buildTree_rec(inorder, 0, n, postorder, 0, n);
     }
 };
