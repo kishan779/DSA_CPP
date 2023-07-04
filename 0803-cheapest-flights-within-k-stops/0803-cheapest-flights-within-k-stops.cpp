ae19a651966882091c6228669dac8477ad0f3be7
KISHAN KUMAR <81502923+kishan779@users.noreply.github.com>
2023-07-04 23:05:56 +0530

    Time: 26 ms (64.34%), Space: 13.6 MB (59.08%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,16 +1,38 @@
 class Solution {
-public://@CodeVed
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-        vector<int> cost(n, INT_MAX);
-        cost[src] = 0;
-        for (int i = 0; i <= K; i++) {
-            vector<int> temp = cost;
-            for (auto f : flights) {
-                if (cost[f[0]] != INT_MAX)
-                    temp[f[1]] = min(temp[f[1]], cost[f[0]] + f[2]);
-            }//@CodeVed
-            cost = temp;
-        }//@CodeVed
-        return cost[dst] != INT_MAX ? cost[dst] : -1;
-    }//@CodeVed
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int, int>>>g(n);
+        for( int i = 0; i< flights.size(); i++){
+          g[flights[i][0]].push_back({flights[i][1], flights[i][2]});
+        }
+
+       priority_queue<tuple<int,int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>>pq;
+
+        vector<int>noOfStops(n, INT_MAX);
+        pq.push({0,src, 0}); // cost :node :stops
+
+        while(!pq.empty()){
+          auto[cost, node, stops] = pq.top();
+          pq.pop();
+
+          if( noOfStops [node] < stops){
+            continue;
+          }
+          noOfStops[node] = stops;
+
+          if(node == dst){
+            return cost;
+          }
+
+          if( stops >k){
+            continue;
+          }
+
+          for( auto adj : g[node]){
+            pq.push({cost + adj.second, adj.first, stops+1});
+          }
+        } 
+
+        return -1;
+    }
 };
