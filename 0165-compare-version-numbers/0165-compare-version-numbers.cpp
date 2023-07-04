8301388181e84d2cf5e62d50cebd3cb54d3bdf3b
KISHAN KUMAR <81502923+kishan779@users.noreply.github.com>
2023-07-05 04:40:38 +0530

    Time: 0 ms (100%), Space: 6.2 MB (64.48%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     int compareVersion(string v1, string v2) {
-        int i = 0, j=0, v1Len = v1.size(), v2Len = v2.size(), n1, n2;
+        // d1 d2 -> store the count of .
 
-        while(i< v1Len || j< v2Len){
-          n1 =0;
-          n2 = 0;
-          while(i < v1Len && v1[i] != '.'){
-            n1 = n1* 10 + (v1[i] - '0'); // '4'-'0' = 4
-            i++;
-          }while(j < v2Len && v2[j] !='.'){
-            n2 = n2 * 10 +(v2[j] - '0');
-            j++;
-          }
-          if(n1<n2) return -1;
-          else if(n1>n2) return 1;
-          i++;
-          j++;
-        }
-        return 0;
+        int d1 = count (v1.begin(), v1.end(), '.');
+        int d2 = count (v2.begin(), v2.end(), '.');
+
+
+            int dif = abs(d1-d2);
+            if(d1>d2){
+              // .0
+              while(dif--) v2 +=".0";
+            }else if(d1<d2){
+              // .0
+              while(dif--) v1+=".0";
+            }
+
+            replace(v1.begin(),v1.end(),'.',' ' );
+            replace(v2.begin(),v2.end(),'.',' ' );
+
+            // StringStream sin(" 1  13  45") sin>>x>>y>>z; x=1,y=13,z=45;
+
+            stringstream ssv1(v1), ssv2(v2);
+
+            int n1, n2;
+
+            while( ssv1>>n1){
+              ssv2>>n2;
+
+              if(n1<n2) return -1;
+              else if(n2<n1) return 1;
+            }
+            return 0;
+
     }
 };
